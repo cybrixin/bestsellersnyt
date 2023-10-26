63a8c72f0cdda93ae0b0e9e8189d5af735822493
Anweshan Roy Chowdhury <anweshanrc15@gmail.com>
2023-10-26 05:56:05 +0530

    Corrections in firebase

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getPerformance } from "firebase/analytics";
+import { getPerformance } from "firebase/performance";
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
